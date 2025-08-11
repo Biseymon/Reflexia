f95978cb01937805b87c623680bba13a515b25a3
SBikin <semyon.bikin@orlan-pdd.ru>
2025-08-11 10:34:14 +0300

    FirstTry

---
+++ Reflexia/PeopleDetector/PeopleDetector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interface;
+
+public class PeopleDetector : IDetector
+{
+    public string Process()
+    {
+        return "Process people detection";
+    }
+}
---
+++ Reflexia/Reflexia/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using Interface;
+
+internal class Program
+{
+    public static string pathToPeopleDet = @"D:\repos\Reflexia\Reflexia\PeopleDetector\bin\Release\net8.0\publish\win-x86\PeopleDetector.dll";
+    private static void Main(string[] args)
+    {
+        
+        Assembly assembly = Assembly.LoadFrom(pathToPeopleDet);
+        Console.WriteLine("Assembly is null? " + (assembly == null).ToString());
+        Type? type = assembly.GetType("PeopleDetector");
+        Console.WriteLine("Type is null? " + (type is null).ToString());
+        if (type != null) 
+        {
+            object peopleDetector = Activator.CreateInstance(type);
+            IDetector iDet = (IDetector)peopleDetector;
+            /*object convertObj = Convert.ChangeType(peopleDetector, typeof(IDetector));
+            IDetector iDet = (IDetector)convertObj;*/
+            Console.WriteLine(iDet.Process());
+        }
+
+
+
+        Console.WriteLine("Hello, World!");
+    }
+}
