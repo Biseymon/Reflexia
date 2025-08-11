09421b334b5cd7abbc9ffa0c9214c33a905a91d9
SBikin <semyon.bikin@orlan-pdd.ru>
2025-08-11 15:46:02 +0300

    Reflaction is works

--- Reflexia/Reflexia/Program.cs
+++ Reflexia/Reflexia/Program.cs
@@ -1,26 +1,16 @@
-﻿using System.Reflection;
-using Interface;
+﻿using Interface;
+using Reflexia;
+using System.Reflection;
 
 internal class Program
 {
-    public static string pathToPeopleDet = @"D:\repos\Reflexia\Reflexia\PeopleDetector\bin\Release\net8.0\publish\win-x86\PeopleDetector.dll";
+    public static string pathToPeopleDet = @"D:\repos\Reflexia\Reflexia\PeopleDetector\bin\Debug\net8.0\PeopleDetector.dll";
     private static void Main(string[] args)
     {
         
-        Assembly assembly = Assembly.LoadFrom(pathToPeopleDet);
-        Console.WriteLine("Assembly is null? " + (assembly == null).ToString());
-        Type? type = assembly.GetType("PeopleDetector");
-        Console.WriteLine("Type is null? " + (type is null).ToString());
-        if (type != null) 
-        {
-            object peopleDetector = Activator.CreateInstance(type);
-            IDetector iDet = (IDetector)peopleDetector;
-            /*object convertObj = Convert.ChangeType(peopleDetector, typeof(IDetector));
-            IDetector iDet = (IDetector)convertObj;*/
-            Console.WriteLine(iDet.Process());
-        }
-
-
+        ReflectionManager reflectionManager = new ReflectionManager();
+        var dets = reflectionManager.GetAllDetector();
+        foreach (var det in dets) { Console.WriteLine(det.Process()); }
 
         Console.WriteLine("Hello, World!");
     }
---
+++ Reflexia/Reflexia/ReflectionManager.cs
@@ -0,0 +1,34 @@
+﻿using Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflexia
+{
+    public class ReflectionManager
+    {
+        public string path = @"D:\repos\Reflexia\Reflexia\PeopleDetector\bin\Debug\net8.0";
+        public IEnumerable<IDetector> GetAllDetector()
+        {
+            var dets = new DirectoryInfo(path).GetFiles("*Detector*.dll");
+            List<Type> types = new();
+            List<IDetector> detectors = new();
+            foreach (var det in dets)
+            {
+                var asm = Assembly.LoadFrom(det.FullName);
+                types.AddRange(asm.GetTypes().Where(t => t.FullName.Contains("Detector") && !t.FullName.Contains("IDetector")).ToArray());
+                
+            }
+            foreach (var type in types)
+            {
+                object peopleDetector = Activator.CreateInstance(type);
+                IDetector iDet = peopleDetector as IDetector;
+                if (iDet != null) detectors.Add(iDet);  
+            }
+            return detectors;
+        }
+    }
+}
